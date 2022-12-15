1ec677628b58258811924bead5cb050323438955
syhxzzz <u202114724@hust.edu.cn>
2022-12-15 23:51:59 +0800

    完成所有内容,还差一个ArrayDeque的测试Test代码

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,89 @@
+public class ArrayDeque <T>{
+    private int size;
+    private int first;
+    private int last;
+    private T[] items;
+    private int capacity=8;
+    public ArrayDeque(){
+        items = (T[]) new Object[capacity];
+        first = last = 0;
+    }
+    public void addFirst(T item){
+        if(isFull()){
+            resize(2*capacity);
+        }
+        first = (first-1+capacity)%capacity;
+        items[first] = item;
+        size+=1;
+    }
+    public void addLast(T item){
+        if(isFull()){
+            resize(2*capacity);
+        }
+        last=(last+1+capacity)%capacity;
+        items[last] = item;
+        size+=1; 
+    }
+    public boolean isEmpty(){
+        return first==last;
+    }
+    public int size(){
+        return size;
+    }
+    public void printDeque(){
+        for(int i=0;i<size-1;i++){
+            int j = (i+first) % capacity;
+            System.out.print(items[j] + " ");
+        }
+        System.out.println(items[last]);
+    }
+    public T removeFirst(){
+        T itemToReturn = items[first];
+        first =(first+1+capacity)%capacity;
+        size-=1;
+        return itemToReturn;
+    }
+    public T removeLast(){
+        T itemToReturn = items[last];
+        last =(last+1+capacity)%capacity;
+        size-=1;
+        if(arrayIsLow()){
+            resize(capacity/2);
+        }
+        return itemToReturn;
+    }
+    public T get(int index){
+        if(index>=size){
+            return null;
+        }
+        int numToReturn;
+        numToReturn = (first + index)%capacity;
+        return items[numToReturn];
+    }
+    public boolean isFull(){
+        return size==capacity;
+    }
+    private boolean arrayIsLow(){
+        return size>=16 && (size/capacity)<0.25;
+    }
+
+    private void resize(int newSize){
+        T[] newArray = (T[]) new Object[newSize];
+        //分成两种情况
+        if(first<last){   //十分正常的平移操作
+            for(int i=0;i<size;i++){
+                newArray[i] = items[first+i];
+            }
+        }
+        else{  //理论上只有first>last
+            for(int i=0;i<size;i++){
+                int j = (first+i)%capacity;
+                newArray[i] = items[j];
+                }
+            }
+        capacity = newSize;
+        first = 0;
+        last = size - 1;
+        items = newArray;
+        }
+}
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,23 +1,40 @@
 public class LinkedListDeque <T>{
-    public int size;
-    public LinkedList <T> sentinel;
-    public class LinkedList <Item>{
-        Item first;
-        LinkedList <Item> next;
-        public LinkedList(Item item1,LinkedList next1){
-            first = item1;
-            next = next1; 
+    private int size;
+    private Node  sentinel;
+    public class Node {
+        T item;
+        Node  next;
+        Node prev;
+        public Node(T item,Node prev,Node next){
+            this.item = item;
+            this.next = next; 
+            this.prev = prev;
         }
     }
-    public LinkedListDeque(T item){
-        sentinel = new LinkedList<T>(item,null);
+    public LinkedListDeque(){
+        sentinel = new Node ((T)new Object() ,null,null);
+        sentinel.prev=sentinel;
+        sentinel.next = sentinel;
+        size = 0;
     }
 
     public void addFirst(T item){ //add remove 操作使用双指针
-
+        Node p1 = sentinel.next;
+        Node p = new Node(item,null,null);
+        sentinel.next = p;
+        p.prev = sentinel;
+        p1.prev = p;
+        p.next = p1;
+        size+=1;
     }
-    public void addlast(T item){
-
+    public void addLast(T item){
+        Node last1 = sentinel.prev;
+        Node newlast1 = new Node(item,null,null);
+        last1.next = newlast1;
+        newlast1.prev=last1;
+        newlast1.next = sentinel;
+        sentinel.prev = newlast1;
+        size+=1;
     }
     public boolean isEmpty(){           
         return  sentinel==sentinel.next;
@@ -26,19 +43,49 @@ public class LinkedListDeque <T>{
         return size;
     }
     public void printDeque(){
-        
+        Node ptr = sentinel.next;
+        while(ptr.next != sentinel){
+            System.out.print(ptr.item + " ");
+            ptr = ptr.next;
+        }
+        System.out.println(ptr.item);
     }
     public T removeFirst(){
-        return null;
+        Node first = sentinel.next;
+        Node second = first.next;
+        first.next=first.prev=null;
+        second.prev = sentinel;
+        sentinel.next = second;
+        size-=1;
+        return first.item;
     }
     public T removeLast(){
-        return null;
+        Node last1 = sentinel.prev;
+        Node last2 = last1.prev;
+        last1.prev = last1.next = null;
+        last2.next = sentinel;
+        sentinel.prev = last2;
+        size-=1;
+        return last1.item;
     }
     public T get(int index){  //get使用迭代而非递归
-        return null;
+        if(index>=size){
+            return null;
+        }
+        Node ptr = sentinel;
+        for(int i=0;i<index;i++){
+            ptr = ptr.next;
+        }
+        return ptr.item;
     }
     public T getRecursive(int index){
-        
-        return null;
+
+        return getRecursiveNode(index, sentinel.next);
+    }
+    private T getRecursiveNode(int index , Node p){
+        if(index==0){
+            return p.item;
+        }
+        return getRecursiveNode(index-1, p.next);
     }
 }
